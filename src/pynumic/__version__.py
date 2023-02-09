fe781ab42d1b6c700189ab1468997efb777a63b4
Oleg Alexandrov <alexandrovoleg.ru@gmail.com>
2023-02-09 17:32:54 +0200

    Add py.typed.

--- src/pynumic/__version__.py
+++ src/pynumic/__version__.py
@@ -2,7 +2,7 @@ import os
 from typing import Iterable
 
 
-VERSION: Iterable[int] = (0, 1, 1)
+VERSION: Iterable[int] = (0, 1, 0)
 __version__: str = ".".join(map(str, VERSION))
 
 poetry_config: str = os.path.join("..", "..", "pyproject.toml")
---
+++ src/pynumic/pynumic.py
@@ -0,0 +1,3 @@
+class Pynumic:
+    """Pynumic.
+    """
